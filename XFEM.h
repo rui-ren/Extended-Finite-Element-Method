ecfe98813568171f36e730f15b47f67771d02c5b
victormaggie <ren_rui@petrochina.com.cn>
2018-01-29 19:04:23 +0000

    Update XFEM.h

--- XFEM.h
+++ XFEM.h
@@ -1,2 +1,3 @@
 # coupling fluid flow and geomechanics
-#
+# Guass-green theorem to decouple the influence of moving boundary.
+# Mass conservation and momentum conservation of pre-existing fracture
